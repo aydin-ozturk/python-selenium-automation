9e1171beae4f6661f59f80ffa60e1b32118aa46e
aydin.ozturk <ozt.aydin@gmail.com>
2022-09-19 21:52:15 -0400

    Amazon Test Case Added

---
+++ Amazon_Test_Case.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.amazon.com")
+driver.maximize_window()
+driver.find_element(By.XPATH, "//a[@href='/gp/css/order-history?ref_=nav_orders_first']").click()
+
+expected_header = "Sign in"
+actual_header = driver.find_element(By.XPATH, "//h1[contains(text(),'Sign in')]").text
+assert expected_header == actual_header, f'Expected {expected_header} but got {actual_header}'
+
+email_input = driver.find_element(By.ID, "ap_email")
+assert email_input, f'Expected Email input field is present but it is not.'
+
+print("Test case passed")
+
+driver.quit()
